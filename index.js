dde9f1255865ea8f417051c6ad4964c40a552a0d
VikiAnimeKpop <167615422+VikiAnimeKpop@users.noreply.github.com>
2024-05-11 13:58:51 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+// let obj = {
+//    c:1,
+//    b:2,
+//    'a':3,
+//    a:0
+// }
+// console.log(obj);
+
+// let obj = {
+//     name:"Viki",
+//     age:20,
+//     admin:true,
+//    'my-login':'jtgggof'
+// }
+// alert(obj.name);
+
+// let week = {
+//    1:'Erkushabti',
+//    2:'ereqshabti',
+//    3:'Choreqshabti',
+//    4:'Hingshabti',
+//    5:'Urbat',
+//    6:'Shabat',
+//    7:'Kiraki'
+// }
+// console.log(week);
+
+//   let arr = [];
+// arr[0] = 'Audi';
+// arr[1] = 2022;
+// arr[2] = '400kg';
+
+// console.log(arr);
+
+// let cars = {};
+// cars.name = 'Audi';
+// cars.model = 2022;
+// cars.weigth = '400kg';
+// console.log(cars);
+
+// let user = {
+//     name:"John",
+//     age:30,
+//     "likes birds" : true
+// };
+
+// let user = {
+//     name:"Viki",
+//     surname:"Avagyan"
+// }
+// console.log(user['name'] + ' ' + user['surname']);
+
+
+// let obj = {
+//     a:1,
+//     b:2,
+//     c:3
+// }
+
+// console.log('a' in obj );//true
+// console.log('1' in obj );//false
+
+// for (let key in obj){
+//     console.log(key);
+//     console.log(obj[key]);
+// }
+
+// let obj = {
+//     name:"Tommy",
+//     '4b':23,
+//     SchoolName:"School number 114",
+//     'surname-nickname':"kiki"
+// }
+// for(let key in obj ){
+//     console.log(obj[key]);
+//     console.log(key);
+// }
